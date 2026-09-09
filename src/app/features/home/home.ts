16cf8546aeeb4ee88c2630f9f5fb6e2f8d32d0bf
Hgo92 <hugo_monier@yahoo.fr>
2026-09-09 18:01:12 +0200

    maj - passage à angular 22

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
 import { Auth } from '../../services/auth';
 import { Router } from '@angular/router';
 
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './home.scss',
 })
 export class Home {
