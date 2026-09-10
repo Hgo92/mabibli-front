fdd864e893b4c3b045e0885625392332ee1aadbe
Hgo92 <hugo_monier@yahoo.fr>
2026-09-10 17:26:42 +0200

    maj - ajout du headbar sur la page library

--- src/app/features/library/library-home/library-home.ts
+++ src/app/features/library/library-home/library-home.ts
@@ -1,8 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Headbar } from '../../headbar/headbar';
 
 @Component({
   selector: 'app-library-home',
-  imports: [],
+  imports: [Headbar],
   templateUrl: './library-home.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './library-home.scss',
