b59349bc45279979acfec27e6cb0c4b1416d2ffd
Hgo92 <hugo_monier@yahoo.fr>
2026-09-10 10:24:22 +0200

    add - ajout des pages register/login/library

--- src/app/features/authentication/register/register.ts
+++ src/app/features/authentication/register/register.ts
@@ -1,11 +1,22 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ChangeDetectionStrategy, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { Auth } from '../../../services/auth';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+  form,
+  FormField,
+  FormRoot,
+  minLength,
+  required,
+  validate,
+  maxLength,
+} from '@angular/forms/signals';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormField, FormRoot],
   templateUrl: './register.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './register.scss',
 })
 export class Register {
@@ -16,4 +27,64 @@ export class Register {
 
   private readonly router = inject(Router);
   private readonly auth = inject(Auth);
+  private snackBar = inject(MatSnackBar);
+
+  registerForm = form(
+    this.registerModel,
+    (schemaPath) => {
+      required(schemaPath.username);
+      minLength(schemaPath.username, 2, { message: 'Nom trop court' });
+      maxLength(schemaPath.username, 15, { message: 'Nom trop long' });
+      validate(schemaPath.username, ({ value }) => {
+        if (value().trim().length === 0) {
+          return {
+            kind: 'whitespace',
+            message: 'Un nom est nécessaire',
+          };
+        }
+        return null;
+      });
+
+      required(schemaPath.password);
+      minLength(schemaPath.password, 8);
+      validate(schemaPath.password, ({ value }) => {
+        if (value().trim().length === 0) {
+          return {
+            kind: 'whitespace',
+            message: 'Mot de passe trop court (8 caractères)',
+          };
+        }
+        return null;
+      });
+    },
+    {
+      submission: {
+        action: async () => {
+          const formValue = this.registerModel();
+
+          this.auth.register(formValue.username, formValue.password).subscribe({
+            next: () => {
+              this.snackBar.open('Votre compte a été créé', 'Fermer', {
+                duration: 3000,
+                panelClass: ['snackbar-success'],
+              });
+              this.auth.login(formValue.username, formValue.password);
+              this.router.navigate(['/library']);
+            },
+            error: (err) => {
+              this.snackBar.open('Erreur dans la création de votre compte', 'Fermer', {
+                duration: 3000,
+                panelClass: ['snackbar-error'],
+              });
+              console.error(err);
+            },
+          });
+        },
+      },
+    },
+  );
+
+  onCancel() {
+    this.router.navigate(['/']);
+  }
 }
--- src/app/features/library/library-home/library-home.ts
+++ src/app/features/library/library-home/library-home.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-library-home',
   imports: [],
   templateUrl: './library-home.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './library-home.scss',
 })
 export class LibraryHome {}
